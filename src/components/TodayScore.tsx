136616d40b429e80cbb28c676ca2baba4b4533a8
milonte <milonte@gmail.com>
2023-03-27 11:24:17 +0200

    feat: TodayScore loading function"

--- src/components/TodayScore.tsx
+++ src/components/TodayScore.tsx
@@ -1,30 +1,50 @@
 import { RadialBar, RadialBarChart, ResponsiveContainer } from 'recharts';
 import '../styles/components/todayscore.scss';
+import { useEffect, useState } from 'react';
 
 export default function TodayScore(data: any) {
-    const score = [{ value: 1 - data.data.todayScore }];
-    const targetAngle = 90 + data.data.todayScore * 360
+    const [isLoading, setIsLoading] = useState(true)
+    const [score, setScore] = useState([{ value: 0 }]);
+    const [targetAngle, setTargetAngle] = useState(0)
+
+    useEffect(() => {
+        console.log(data.data.todayScore)
+        if (data.data.todayScore !== undefined) {
+            setScore([{ value: 1 - data.data.todayScore }]);
+            setTargetAngle(90 + data.data.todayScore * 360);
+            setIsLoading(false);
+        }
+
+    }, [data.data.todayScore]);
+
 
     return (
         <div id="today-score">
-            <h2 className="title">Score</h2>
-            <span className='legend'>
-                <p className='percent'>{Number(data.data.todayScore) * 100}%</p>
-                <p>de votre objectif</p>
-            </span>
-            <div className='r'></div>
-            <ResponsiveContainer>
-                <RadialBarChart
-                    innerRadius="65%"
-                    outerRadius="75%"
-                    data={score}
-                    startAngle={90}
-                    endAngle={targetAngle}
-                    barSize={20}
-                >
-                    <RadialBar fill='#E60000' cornerRadius={20} background dataKey='value' />
-                </RadialBarChart>
-            </ResponsiveContainer>
+            {isLoading ? (
+                <>Loading</>
+            ) : (
+                <>
+                    <h2 className="title">Score</h2>
+                    <span className='legend'>
+                        <p className='percent'>{Number(data.data.todayScore) * 100}%</p>
+                        <p>de votre objectif</p>
+                    </span>
+                    <div className='r'></div>
+                    <ResponsiveContainer>
+                        <RadialBarChart
+                            innerRadius="65%"
+                            outerRadius="75%"
+                            data={score}
+                            startAngle={90}
+                            endAngle={targetAngle}
+                            barSize={20}
+                        >
+                            <RadialBar fill='#E60000' cornerRadius={20} background dataKey='value' />
+                        </RadialBarChart>
+                    </ResponsiveContainer>
+                </>
+            )}
         </div>
+
     );
 }
