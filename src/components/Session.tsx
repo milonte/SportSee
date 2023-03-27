eedbc173e69a6e0a11f456c20ceec48320404280
milonte <milonte@gmail.com>
2023-03-27 11:50:29 +0200

    feat: Sessions loader function

--- src/components/Session.tsx
+++ src/components/Session.tsx
@@ -1,8 +1,19 @@
+import { useEffect, useState } from 'react';
 import '../styles/components/session.scss';
 import { Line, LineChart, ResponsiveContainer, Tooltip, XAxis, YAxis } from "recharts";
+import Loader from './Loader';
 
 export default function Session(data: any) {
-    const sessions = data.data.sessions;
+    const [isLoading, setIsLoading] = useState(true)
+    const [sessions, setSessions] = useState([]);
+
+    useEffect(() => {
+        if (data.data.sessions !== undefined) {
+            setSessions(data.data.sessions)
+            setIsLoading(false);
+        }
+
+    }, [data.data.sessions]);
 
     const formatDay = (data: any) => {
         switch (data.day) {
@@ -18,33 +29,39 @@ export default function Session(data: any) {
 
     return (
         <div id='sessions'>
-            <h2 className='title'>Durée moyenne des sessions</h2>
-            <ResponsiveContainer>
-                <LineChart
-                    data={sessions}
-                    margin={{
-                        top: 120,
-                        right: 20,
-                        left: 20,
-                        bottom: 20,
-                    }}
-                >
-                    <XAxis dataKey={(data) => formatDay(data)} axisLine={false} tickLine={false} tick={{ fill: 'white' }} />
-                    <YAxis dataKey="sessionLength" hide allowDataOverflow={true} />
-                    <Tooltip content={({ active, payload }): any => {
-                        if (active && payload && payload.length) {
-                            return (
-                                <div className="session-tooltip">
-                                    <p className="label">{`${payload[0].value} min`}</p>
-                                </div>
-                            );
-                        }
+            {isLoading ? (
+                <Loader />
+            ) : (
+                <>
+                    <h2 className='title'>Durée moyenne des sessions</h2>
+                    <ResponsiveContainer>
+                        <LineChart
+                            data={sessions}
+                            margin={{
+                                top: 120,
+                                right: 20,
+                                left: 20,
+                                bottom: 20,
+                            }}
+                        >
+                            <XAxis dataKey={(data) => formatDay(data)} axisLine={false} tickLine={false} tick={{ fill: 'white' }} />
+                            <YAxis dataKey="sessionLength" hide allowDataOverflow={true} />
+                            <Tooltip content={({ active, payload }): any => {
+                                if (active && payload && payload.length) {
+                                    return (
+                                        <div className="session-tooltip">
+                                            <p className="label">{`${payload[0].value} min`}</p>
+                                        </div>
+                                    );
+                                }
 
-                        return null;
-                    }} />
-                    <Line type="monotoneX" dataKey="sessionLength" stroke="#FFFFFF" strokeWidth={2} dot={false} />
-                </LineChart>
-            </ResponsiveContainer>
+                                return null;
+                            }} />
+                            <Line type="monotoneX" dataKey="sessionLength" stroke="#FFFFFF" strokeWidth={2} dot={false} />
+                        </LineChart>
+                    </ResponsiveContainer>
+                </>
+            )}
         </div>
     );
 }
