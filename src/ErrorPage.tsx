d3385c8f776f6212544b0cdf98103da32cd42596
milonte <milonte@gmail.com>
2023-04-09 14:08:19 +0200

    ErrorPage style

--- src/ErrorPage.tsx
+++ src/ErrorPage.tsx
@@ -19,12 +19,8 @@ export default function ErrorPage(): ReactElement {
             <div className="main">
                 <div id="error">
                     <div className='error-status'>{error.status}</div>
-                    {/* <div className='error-message'>{404 === error.status ?
-                        "Oups! La page que vous demandez n'existe pas." :
-                        "Oups! Un problème est survenu."}
-                    </div> */}
                     <div className='error-message'>{error.statusText}</div>
-                    <Link to={"/"} className='home-link'>Retourner sur la page d’accueil</Link>
+                    <Link to={"#"} className='home-link'>Retourner sur la page d’accueil</Link>
                 </div>
             </div>
         </>
